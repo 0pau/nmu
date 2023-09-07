6bd188f92999f7d8b8fe5a3b53958b9cf301e8d0
0pau <opauszkinorbi1@gmail.com>
2023-09-07 18:39:16 +0200

    NMU CSS betöltése

--- nmu.user.js
+++ nmu.user.js
@@ -1,13 +1,21 @@
 // ==UserScript==
 // @name         Neptun MakeUp
-// @namespace    http://tampermonkey.net/
-// @version      0.1
+// @namespace    https://github.com/0pau/nmu/
+// @version      0.2a
 // @description  Egy kis smink nem árthat a Neptunnak se...
 // @author       0pau
 // @include      https://*neptun*/hallgato*/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
-// @downloadURL  https://github.com/0pau/nmu/releases/latest/download/nmu.js
+// @downloadURL  https://github.com/0pau/nmu/releases/latest/download/nmu.user.js
 // @grant        none
 // ==/UserScript==
 
-console.log("Tesztüzenet! A Neptun MakeUp kisminkelte a Neptunodat.");
+const style = document.createElement("style");
+
+var xhr = new XMLHttpRequest;
+xhr.open("GET", "https://raw.githubusercontent.com/0pau/nmu/main/nmu.css");
+xhr.addEventListener("load", ()=>{
+    style.innerHTML = xhr.responseText;
+});
+xhr.send();
+document.body.append(style);
