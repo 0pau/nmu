e422f0a1c517181aaa18fe6c293b2d9b0d8eb783
0pau <opauszkinorbi1@gmail.com>
2023-09-07 18:21:21 +0200

    Create nmu.js

---
+++ nmu.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Neptun MakeUp
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Egy kis smink nem árthat a Neptunnak se...
+// @author       0pau
+// @include      https://*neptun*/hallgato*/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @downloadURL  https://github.com/0pau/nmu/releases/latest/download/nmu.js
+// @grant        none
+// ==/UserScript==
+
+console.log("Tesztüzenet! A Neptun MakeUp kisminkelte a Neptunodat.");
